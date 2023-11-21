ac52910b1e8ab30ab552c9fe86a1c1518aa1cae5
Mustafa ALAN <mustafa.alann@hotmail.com>
2023-11-21 03:22:48 +0300

    Trap4

---
+++ Assets/Scripts/MovingUpTrap.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingUpTrap : MonoBehaviour
+{
+    public float moveSpeed = 5f;    // Speed of the movement
+
+    void Start()
+    {
+        StartCoroutine(MoveUpDown());
+    }
+
+    IEnumerator MoveUpDown()
+    {
+        while (true)
+        {
+            // Move up
+            while (transform.position.y < -1)
+            {
+                transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            // Wait for 2 seconds
+            yield return new WaitForSeconds(2f);
+
+            // Move down
+            while (transform.position.y > -3.2)
+            {
+                transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            // Wait for 2 seconds
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -15,7 +15,7 @@ public class PlayerMove : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         // let the gameObject fall down
-        gameObject.transform.position = new Vector3(45, 3, 45);
+        gameObject.transform.position = new Vector3(4, 3, 5);
     }
 
     void Update()
